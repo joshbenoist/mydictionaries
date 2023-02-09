4d7e8cc06fe98de5235d50856cea6dbf767d18a3
Josh <josh_benoist1@baylor.edu>
2023-02-08 19:36:54 -0600

    Dictionaries HW

--- 1_dictionary start file.py
+++ 1_dictionary start file.py
@@ -5,13 +5,16 @@ phonebook = {'Chris':'555−1111',
              'Joanne':'555−3333'}
 
 
-
 print()
 print('*****  start section 1 - print dictionary ********')
 print()
 
 
+print(phonebook)
 
+phonebook['Chris'] = "555-4444"
+
+phonebook[Joe] = "555-4444"
 
 
 print()
@@ -19,7 +22,7 @@ print('*****  end section 1 ********')
 print()
 
 
-'''
+
 
 
 print()
@@ -63,7 +66,7 @@ print()
 print('*****  start section 4 - delete/remove from dictionary ********')
 print()
 
-
+print(phonebook)
 
 
 print()
@@ -71,7 +74,7 @@ print('*****  end section 4 ********')
 print()
 
 
-
+''''
 
 
 
@@ -80,7 +83,14 @@ print('*****  start section 5 - iterate through keys, values, items ********')
 print()
 
 
+for key in phonebook:
+    print(f"The key is: {key} and the value is {phonebook[key]}")
 
+for value in phonebook.values():
+    print(value)
+
+for k, v in phonebook.items():
+    print(f"The key is: {k} and the value is {v}")
 
 
 
@@ -97,7 +107,11 @@ print('*****  start section 6 - using get and clear ********')
 print()
 
 
+name = "Chris"
 
+phone = phonebook.get(name, "key not found")
+
+print(phone)
 
 
 
@@ -112,8 +126,10 @@ print('*****  start section 7 - using pop method ********')
 print()
 
 
+remove = phonebook.pop("Chris", "not found")
 
-
+print(remove)
+print(phonebook)
 
 
 print()
@@ -127,7 +143,11 @@ print('*****  start section 8 - using popitem ********')
 print()
 
 
+a = phonebook.popitem()
 
+print(a)
+
+print(phonebook)
 
 
 
@@ -135,15 +155,19 @@ print()
 print('*****  end section 8 ********')
 print()
 
-
+'''
 
 print()
 print('*****  start section 9 - using random and converting to list ********')
 print()
 
 
+list_of_keys = list(phonebook)
+random_key = random.choice(list_of_keys)
+print(random_key)
+print(phonebook[random_key])
 
-
+print(phonebook[random.choice(list(phonebook))])
 
 print()
 print('*****  end section 9 ********')
--- 2_person_dict.py
+++ 2_person_dict.py
@@ -5,3 +5,23 @@ person["age"] = 51
 person["spouse"] = "Edna"
 person["children"] = ["Ralph", "Betty", "Joey"]
 person["pets"] = {"dog": "Fido", "cat": "Sox"}
+
+print(person)
+
+
+# the name of the second child
+print(person.get("children")[1])
+print(person["children"])
+
+# the name of the cat
+print(type(person))
+
+
+# use a for loop to list each child
+for i in person["children"]:
+
+
+# print out the pets in this format;
+# 'type of pet: dog name of pet: Fido'
+
+for 
